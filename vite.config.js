28fa145b4c262fcbb6e59f02f3b2769bb2d60f2c
seif2303948 <seif.984002@gmail.com>
2026-04-02 04:52:23 +0200

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   // https://unhospitalized-rohan-prototypic.ngrok-free.dev                                  
-  base: "/ecommerce-project",
+  base: "/ecommerce-project/",
   plugins: [react()],
   server :{
     proxy:{
